34f9bd8656f66a615bbceb6740ac032f62fa4d55
Evan Hazlett <ejhazlett@gmail.com>
2013-03-04 03:26:41 -0500

    updated provision script

--- provision.sh
+++ provision.sh
@@ -5,7 +5,6 @@ apt-get -y update 2>&1 > /dev/null
 apt-get -y install vim screen python-setuptools python-dev
 easy_install pip > /dev/null 2>&1
 
-echo "Configuring Puppet master"
 DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential squashfs-tools debootstrap genisoimage syslinux squid-deb-proxy squid-deb-proxy-client
 
 exit 0
